cc7fcb9feede05d77d5d82aee9b2749c5382210c
aelamran <ayyoub.elamrani4@gmail.com>
2019-10-19 16:50:16 +0200

    BFS method

--- deliberative/src/deliberativeAgent/DeliberativeAgent.java
+++ deliberative/src/deliberativeAgent/DeliberativeAgent.java
@@ -83,108 +83,7 @@ public class DeliberativeAgent implements DeliberativeBehavior {
 			plan = aStarPlan(vehicle, tasks);
 			break;
 		case BFS:
-			int V = tasks.size();
-			int i =0;
-			State initialState = new State(vehicle.getCurrentTasks(), tasks, vehicle.getCurrentCity(), i);
-			i++;
-			
-			List<State> visitedState = new ArrayList<State>();
-			LinkedList<State> queue = new LinkedList<State>();
-			LinkedList<City> cities = new LinkedList<City>();
-			HashMap <Integer, State> parentStates = new HashMap<Integer, State>();
-			HashMap <Integer, Action> parentAction = new HashMap<Integer, Action>();
-			LinkedList<Action> bestPath = new LinkedList<Action>();
-			double tmpCost = 0.0;
-			double finalCost = Double.MAX_VALUE;
-			visitedState.add(initialState);
-			queue.add(initialState);
-			
-			while(queue.size() != 0)
-			{
-				
-				initialState = queue.poll();				
-				if(initialState.isGoalState()){
-					
-					LinkedList<Action> path = new LinkedList<Action>();
-					State parentState = initialState;
-					State interState = initialState;
-					while(parentStates.containsKey(parentState.getId())){
-						Action toAdd = parentAction.get(parentState.getId());
-						path.add(0, toAdd);
-						cities.add(0, parentState.getCurrentCity());
-						System.out.println(parentAction.get(parentState.getId()));
-						tmpCost += vehicle.costPerKm()*interState.getCurrentCity().distanceTo(parentState.getCurrentCity());
-						parentState = parentStates.get(parentState.getId());
-						interState = parentState;
-					}
-					if(tmpCost<finalCost) {
-						finalCost = tmpCost;
-						bestPath = path;
-					}
-					
-				}
-				
-				
-				List<State> neighboursState = new ArrayList<State>();
-				for (Task task: initialState.getCurrentTasks()) {
-					TaskSet carriedTasks = initialState.getCurrentTasks().clone();
-					carriedTasks.remove(task);
-					State tmpState = new State(carriedTasks, initialState.getAvailableTasks(), task.deliveryCity, i);
-					neighboursState.add(tmpState);
-					
-					parentStates.put(i, initialState);
-					parentAction.put(i, new Delivery(task));
-					i++;	
-				}
-				
-				for(Task task: initialState.getAvailableTasks()) {
-					if(vehicle.capacity() >= initialState.getCurrentTasks().weightSum()+ task.weight) {
-						
-						TaskSet carriedTasks = initialState.getCurrentTasks().clone();
-						TaskSet availableTasks = initialState.getAvailableTasks().clone();				
-						availableTasks.remove(task);
-						carriedTasks.add(task);
-						State tmpState = new State(carriedTasks, availableTasks, task.pickupCity, i);
-						neighboursState.add(tmpState);
-						parentStates.put(i, initialState);
-						parentAction.put(i, new Pickup(task));
-						i++;
-						
-					}
-				}
-				
-
-				
-				for(State state: neighboursState) {
-					if(!visitedState(visitedState, state)) {
-						visitedState.add(state);
-						queue.add(state);
-						System.out.println("visited state");
-						//System.out.println(state.getId());
-					}
-				}
-				
-			}
-			
-			
-			
-			
-			// ...
-			
-			
-			City oldCity = vehicle.getCurrentCity();
-			plan = new Plan(oldCity);
-			for (int j=0; j<bestPath.size(); j++) {
-				Action action = bestPath.get(j);
-				for (City city : oldCity.pathTo(cities.get(j))) {
-					plan.appendMove(city);
-				}
-				oldCity = cities.get(j);
-				plan.append(action);
-				
-			}
-
-			
+			plan = bfsPlan(vehicle, tasks);	
 			break;
 		default:
 			throw new AssertionError("Should not happen.");
@@ -193,6 +92,105 @@ public class DeliberativeAgent implements DeliberativeBehavior {
 		return plan;
 	}
 	
+	private Plan bfsPlan(Vehicle vehicle, TaskSet tasks) {
+		
+		Plan plan = new Plan(vehicle.getCurrentCity());
+		int i =0;
+		State initialState = new State(vehicle.getCurrentTasks(), tasks, vehicle.getCurrentCity(), i);
+		i++;
+		
+		List<State> visitedState = new LinkedList<State>();
+		LinkedList<State> queue = new LinkedList<State>();
+		
+		HashMap <Integer, State> parentStates = new HashMap<Integer, State>();
+		HashMap <Integer, Action> parentAction = new HashMap<Integer, Action>();
+		LinkedList<Action> bestPath = new LinkedList<Action>();
+		LinkedList<City> bestCitiesPath = new LinkedList<City>();
+		double tmpCost = 0.0;
+		double finalCost = Double.MAX_VALUE;
+		visitedState.add(initialState);
+		queue.add(initialState);
+		
+		while(queue.size() != 0)
+		{
+			
+			initialState = queue.poll();				
+			if(initialState.isGoalState()){
+				
+				LinkedList<Action> path = new LinkedList<Action>();
+				LinkedList<City> cities = new LinkedList<City>();
+				State parentState = initialState;
+				State interState = initialState;
+				while(parentStates.containsKey(parentState.getId())){
+					Action toAdd = parentAction.get(parentState.getId());
+					path.add(0, toAdd);
+					cities.add(0, parentState.getCurrentCity());
+					tmpCost += vehicle.costPerKm()*interState.getCurrentCity().distanceTo(parentState.getCurrentCity());
+					parentState = parentStates.get(parentState.getId());
+					interState = parentState;
+				}
+				if(tmpCost<finalCost) {
+					finalCost = tmpCost;
+					bestPath = path;
+					bestCitiesPath = cities;
+				}
+				
+			}
+			
+			
+			List<State> neighboursState = new ArrayList<State>();
+			for (Task task: initialState.getCurrentTasks()) {
+				TaskSet carriedTasks = initialState.getCurrentTasks().clone();
+				carriedTasks.remove(task);
+				State tmpState = new State(carriedTasks, initialState.getAvailableTasks(), task.deliveryCity, i);
+				neighboursState.add(tmpState);
+				
+				parentStates.put(i, initialState);
+				parentAction.put(i, new Delivery(task));
+				i++;	
+			}
+			
+			for(Task task: initialState.getAvailableTasks()) {
+				if(vehicle.capacity() >= initialState.getCurrentTasks().weightSum()+ task.weight) {
+					
+					TaskSet carriedTasks = initialState.getCurrentTasks().clone();
+					TaskSet availableTasks = initialState.getAvailableTasks().clone();				
+					availableTasks.remove(task);
+					carriedTasks.add(task);
+					State tmpState = new State(carriedTasks, availableTasks, task.pickupCity, i);
+					neighboursState.add(tmpState);
+					parentStates.put(i, initialState);
+					parentAction.put(i, new Pickup(task));
+					i++;
+					
+				}
+			}
+			
+
+			
+			for(State state: neighboursState) {
+				if(!visitedState(visitedState, state)) {
+					visitedState.add(state);
+					queue.add(state);
+				}
+			}
+			
+		}		
+		
+		City oldCity = vehicle.getCurrentCity();
+		plan = new Plan(oldCity);
+		for (int j=0; j<bestPath.size(); j++) {
+			Action action = bestPath.get(j);
+			for (City city : oldCity.pathTo(bestCitiesPath.get(j))) {
+				plan.appendMove(city);
+			}
+			oldCity = bestCitiesPath.get(j);
+			plan.append(action);
+			
+		}
+		
+		return plan;
+	}
 
 
 	private Plan aStarPlan(Vehicle vehicle, TaskSet tasks) {
